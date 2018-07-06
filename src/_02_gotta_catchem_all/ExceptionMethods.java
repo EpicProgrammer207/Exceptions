fdc713fb2cd9881913924b99b769221527c62392
BenProgramsThings <league@WTS14.attlocal.net>
2018-07-05 19:16:26 -0700

    dun... me now knows why null pointers are the death of us

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,24 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	static String reverseString(String s) {
+		if (s.equals("")) {
+			throw new IllegalStateException();
+		} else {
+			String s2 = "";
+			char[] c = s.toCharArray();
+			for (int i = c.length - 1; i >= 0; i--) {
+				s2 += c[i];
+			}
+			return s2;
+		}
+	}
+
+	static double divide(double a, double b) {
+		if (b == 0.0) {
+			throw new IllegalArgumentException();
+		} else {
+			return a / b;
+		}
+	}
 }
